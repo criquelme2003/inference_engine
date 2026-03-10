ee797efbb762e516d2a45c3c7c8ca98a03ac0ac2
criquelme2003 <carlitisamuel@gmail.com>
2026-03-10 23:11:57 +0000

    fix: main

--- src/main.cu
+++ src/main.cu
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include "onnx/onnx-ml.pb.h"
 
-__global__
-void test_kernel()
-{
-}
+int main() {
 
-int main()
-{
-    test_kernel<<<1,1>>>();
-    cudaDeviceSynchronize();
+    onnx::ModelProto model;
 
-    std::cout << "CUDA project compiled successfully!" << std::endl;
+    std::ifstream input("model.onnx", std::ios::binary);
+
+    if (!model.ParseFromIstream(&input)) {
+        std::cout << "Error loading ONNX\n";
+        return 1;
+    }
+
+    std::cout << "Nodes: " << model.graph().node_size() << std::endl;
 
     return 0;
 }
