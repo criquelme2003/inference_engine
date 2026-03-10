a152d785f969c6037cc0612a6de83bdc347d36a9
criquelme2003 <carlitisamuel@gmail.com>
2026-03-10 23:07:41 +0000

    feat: onnx config  & protobuf

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,8 @@ RUN apt-get update && apt-get install -y \
     build-essential \
     python3-pybind11 \
     pybind11-dev \
+    libprotobuf-dev \
+    protobuf-compiler
     && rm -rf /var/lib/apt/lists/* 
 
 WORKDIR /workspace
@@ -23,3 +25,4 @@ COPY . .
 
 RUN mkdir extern && cd extern && git clone --depth 1 https://github.com/dmlc/dlpack.git
 
+RUN git clone https://github.com/onnx/onnx.git && cd onnx && git submodule update --init --recursive && mkdir build && cd build && cmake .. && make
--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-import forgethreads
-print(forgethreads.test())
+import tensorflow as tf
+import tf2onnx
+import onnx
+
+model = tf.keras.Sequential([
+    tf.keras.Input(shape=(3,)),
+    tf.keras.layers.Dense(4, activation="relu"),
+    tf.keras.layers.Dense(2),
+    tf.keras.layers.Softmax()
+])
+
+@tf.function
+def forward(x):
+    return model(x)
+
+input_signature = [tf.TensorSpec([None, 3], tf.float32)]
+
+onnx_model, _ = tf2onnx.convert.from_function(
+    forward,
+    input_signature=input_signature,
+    opset=13
+)
+
+onnx.save(onnx_model, "/workspace/models/model.onnx")
